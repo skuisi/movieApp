ed6bc1f58923e1831ce2780d044058f370155b2b
skuisi <iskuisi@gmail.com>
2021-08-26 12:00:14 +0100

    first commit

---
+++ src/components/MovieCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Card} from "react-bootstrap"
+import ReactStars from "react-rating-stars-component";
+
+const MovieCard = ({Movie}) => {
+    return (
+        <div style={{color: 'black'}}>
+           <Card style={{ width: '18rem' }}>
+  <Card.Img alt= {Movie.name} variant="top" src={Movie.posterurl} />
+  <Card.Body>
+    <Card.Title>{Movie.name}</Card.Title>
+    <Card.Text>
+      {Movie.description}
+    </Card.Text>
+    <ReactStars
+    count={5}
+    //onChange={ratingChanged}
+    value={Movie.rating}
+    edit= {false}
+    size={24}
+    activeColor="#ffd700"
+  />
+  </Card.Body>
+</Card>
+        </div>
+    );
+}
+
+export default MovieCard;
